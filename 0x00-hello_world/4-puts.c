6819ca772df3171a49a44f1a2a8f26d6d69364eb
Mortaja <mortaja2000_@hotmail.com>
2023-08-10 19:21:04 -0700

    My first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,13 @@
 /**
  * main - Entry point
  *
- * Description: A C program that prints with put function
+ * Description: A C program that prints with put fuction
  *
- * Return: Always 0 (Success)
+ * Return: Alawys 0 (Success)
 */
 
 int main(void)
 {
- Puts("\"Programming is like building a multilingual puzzle");
- return (0);
-} 
+	puts("\"Programming is like building a multilingual puzzle");
+       return (0);
+}	
