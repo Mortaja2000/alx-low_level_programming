9d53732a36a5182c02bfef28c3b6ad61c6bb0a6c
Mortaja <mortaja2000_@hotmail.com>
2023-08-15 17:49:27 -0700

    My second commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,13 +9,11 @@
  * Return: Always 0 (Success)
 */
 
-int main(void)
+void print_alphabet(void)
 {
 	int ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 		_putchar(ch);
 	_putchar('\n');
-
-	return (0);
 }
