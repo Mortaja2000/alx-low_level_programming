b10a549984f21cb30e85ee274c05f0d5c65c7411
Mortaja <mortaja2000_@hotmail.com>
2023-08-24 15:44:51 -0700

    My second commit

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+/**
+ * magic - prints a[2] = 98 followed by anew line*/
+
 int main(void)
 {
 	int n;
@@ -8,17 +11,17 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-	/**
-	* write your code of line here...
-	* Remember:
-	* - you are not allowd to use a
-	* - you are not allowed to modify p
-	* - only one statement
-	* - you are not allowed to code anything else than this line of code
-	*/
+/**
+* write your code of line here...
+* Remember:
+* - you are not allowd to use a
+* - you are not allowed to modify p
+* - only one statement
+* - you are not allowed to code anything else than this line of code
+*/
 	*(p + 5) = 98;
 	/*...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
-	return (0);
+return (0);
 
 }
