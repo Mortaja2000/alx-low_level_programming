d2fdba15e874aabb5168771a1ce2283e19df18f2
Mortaja <mortaja2000_@hotmail.com>
2023-10-06 18:21:19 -0700

    My second commit

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,6 +3,8 @@
 
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/stat.h>
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -11,6 +13,7 @@
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
+int _strlen(char *str);
 int append_text_to_file(const char *filename, char *text_content);
 
 #endif
